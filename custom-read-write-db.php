5fcb285330f61879505e068cd05bd2efe1d90c29
Hiroshi Ozeki <ozeki.hiroshi@gmail.com>
2024-04-11 23:31:02 +0200

    filter

--- custom-read-write-db.php
+++ custom-read-write-db.php
@@ -5,43 +5,49 @@ Description: WordPress plugin to separate read and write database connections.
 Version: 1.0
 Author: Hiroshi Ozeki
 */
+// Define custom database class
+class Custom_Read_Write_DB extends wpdb {
+    public $write_connection;
+    public $read_connection;
 
-// プラグインのメインクラス
-class Custom_Read_Write_DB_Plugin {
-    // コンストラクタ
-    public function __construct() {
-        // フックを設定
-        register_activation_hook(__FILE__, array($this, 'activate'));
+    function __construct() {
+        // Initialize the master database (write connection)
+        $this->write_connection = new wpdb(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
+
+        // Initialize the read database
+        $this->read_connection = new wpdb(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST_READ);
     }
 
-    // プラグインが有効化されたときに実行される関数
-    public function activate() {
-        // 初期化処理を実行
-        $this->init();
+    // Custom query routing logic
+    function query($query) {
+        // Log the query for debugging
+        error_log('Custom_Read_Write_DB: Query: ' . $query);
+
+        // Determine if query is a read operation
+        if ($this->is_read_query($query)) {
+            // Log that the query is sent to the read database
+            error_log('Custom_Read_Write_DB: Sending query to read database');
+
+            // Execute the query on the read connection
+            return $this->read_connection->query($query);
+        } else {
+            // Log that the query is sent to the write database
+            error_log('Custom_Read_Write_DB: Sending query to write database');
+
+            // Execute the query on the write connection (master database)
+            return $this->write_connection->query($query);
+        }
     }
 
-    // プラグインの初期化
-    public function init() {
-        // Custom_Read_Write_DBクラスをロードし、wpdbオブジェクトを置き換える
-        require_once(plugin_dir_path(__FILE__) . 'custom-read-write-db-class.php');
-        global $wpdb;
-        $wpdb = new Custom_Read_Write_DB();
-
-        // ここでデータベース接続を設定する
-        $wpdb->add_database(array(
-            'host'     => DB_HOST_WRITE,
-            'user'     => DB_USER,
-            'password' => DB_PASSWORD,
-            'name'     => DB_NAME,
-        ));
-        $wpdb->add_database(array(
-            'host'     => DB_HOST_READ,
-            'user'     => DB_USER,
-            'password' => DB_PASSWORD,
-            'name'     => DB_NAME,
-        ));
+    // Logic to determine if a query is for reading
+    function is_read_query($query) {
+        // Check if the query starts with "SELECT" or contains "DESCRIBE" or "EXPLAIN"
+        return (preg_match('/^\s*SELECT|DESCRIBE|EXPLAIN/i', $query) === 1);
     }
 }
 
-// インスタンス化してプラグインを開始
-new Custom_Read_Write_DB_Plugin();
+// Filter to return custom database class instance
+function custom_read_write_db_init($wpdb) {
+    return new Custom_Read_Write_DB();
+}
+add_filter('wpdb', 'custom_read_write_db_init');
